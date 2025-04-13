f782d9c2e7b1acf3ba71e90839397794e840a1a2
non022545 <nontthont7@gmail.com>
2025-04-13 17:55:58 +0700

    dasdsa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import '@fontsource/nosifer'; // default 400 weight
 
 function Navbar() {
     return (
         <>
             <nav>
                 <div className='font-bold text-2xl flex fixed h-20 w-full z-50 mt-0 shadow items-center justify-between py-4 px-64 bg-gradient-to-br from-purple-800 via-purple-950 to-gray-900 text-white'>
-                    <div>NvaloShop</div>
+                    <div className='font-nosifer text-4xl'>
+                        <p>NvaloShop</p>
+                    </div>
                     <div className='flex gap-x-10'>
                         <Link className='hover:text-pink-700' to="/">Home</Link>
                         <Link className='hover:text-pink-700' to="/Shop">Shop</Link>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -7,26 +7,29 @@ function Home() {
     return (
         <div>
             <Navbar />
+            <div className="h-auto min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('https://images.alphacoders.com/136/thumb-1920-1360645.jpeg')" }}>
 
-            <div className="h-auto min-h-screen bg-cover bg-center text-white px-6 py-12 flex flex-col items-center justify-center text-center" style={{ backgroundImage: "url('https://images7.alphacoders.com/136/thumbbig-1369073.webp')" }}>
-                <div className="flex flex-col items-center justify-center  bg-black/80 backdrop-blur-md h-96 w-auto rounded-xl shadow-lg text-white">
-                    <h1 className="text-5xl font-extrabold mb-4">
-                        Welcome to <span className="text-pink-800">Nvaloshop</span>
-                    </h1>
+                <div className="h-auto min-h-screen bg-cover bg-center text-white px-6 py-12 flex flex-col items-center justify-center text-center">
+                    <div className="flex flex-col items-center justify-center  bg-black/80 backdrop-blur-md h-96 w-auto rounded-xl shadow-lg text-white">
+                        <h1 className="text-5xl font-extrabold mb-4">
+                            Welcome to <span className="text-pink-800">Nvaloshop</span>
+                        </h1>
 
-                    <p className="text-lg max-w-2xl mb-8 text-gray-300">
-                        🎯 เว็บขายไอดี Valorant คุณภาพ ราคาประหยัด พร้อมสกินเทพ Rank สูง และบริการหลังการขายระดับเทพ 🔥
-                    </p>
+                        <p className="text-lg max-w-2xl mb-8 text-gray-300">
+                            🎯 เว็บขายไอดี Valorant คุณภาพ ราคาประหยัด พร้อมสกินเทพ Rank สูง และบริการหลังการขายระดับเทพ 🔥
+                        </p>
 
 
-                    <Link to="/shop">
-                        <button className="bg-pink-800 hover:bg-pink-900 text-white px-6 py-3 rounded-full text-lg shadow-lg transition-all">
-                            เข้าหน้าร้านค้า ➜
-                        </button>
-                    </Link>
+                        <Link to="/shop">
+                            <button className="bg-pink-800 hover:bg-pink-900 text-white px-6 py-3 rounded-full text-lg shadow-lg transition-all">
+                                เข้าหน้าร้านค้า ➜
+                            </button>
+                        </Link>
 
+                    </div>
                 </div>
             </div>
+
             <Footer />
 
         </div>
